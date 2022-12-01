bd6ce874444feb30b9180f93ea97d831d3530c1c
Nooooooaah <noahschaefer19@gmail.com>
2022-12-01 14:21:48 +0100

    Created Setup file

---
+++ setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Dec  1 14:07:44 2022
+
+@author: noahs
+"""
+
+from setuptools import setup
+
+setup(
+    name='DSSS_Homework_Repository',
+    version='0.1.0',    
+    description='A code which creates colorfull snowflakes',
+    url='https://github.com/Nooooooaah/DSSS_Homework_Repository.git',
+    author='Noah Schäfer',
+    author_email='noah.schaefer@fau.de',
+    license='Apache License 2.0',
+    packages=[''],
+    install_requires=['turtle',
+                      'numpy', 'random','seaborn'                    
+                      ],
+
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: DSSS Tutors/ Researchers',
+        'License :: Apache License 2.0',  
+        'Operating System :: Windows',        
+        'Programming Language :: Python :: 3.9',
+    ],
+)
