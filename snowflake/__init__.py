439d31044993331b56eec8e87d83ab3864b067f7
Nooooooaah <noahschaefer19@gmail.com>
2022-12-01 14:38:42 +0100

    Innit file

---
+++ snowflake/__init__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Dec  1 14:32:50 2022
+
+@author: noahs
+"""
+
+"""
+DSSS_Homework_Repository
+
+An Snowflake creating code.
+"""
+
+__version__ = "0.1.0"
+__author__ = 'Noah Schäfer'
+__credits__ = 'Friedrich Alexander University Erlangen-Nürnberg'
